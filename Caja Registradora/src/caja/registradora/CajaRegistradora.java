f42756aeed2424ee736b9f97c6ae3e3292ca5e24
Nestor Alexander <nalexcastaneda@gmail.com>
2019-11-05 18:37:10 -0600

    Update CajaRegistradora.java

--- Caja Registradora/src/caja/registradora/CajaRegistradora.java
+++ Caja Registradora/src/caja/registradora/CajaRegistradora.java
@@ -21,13 +21,13 @@ public class CajaRegistradora {
     int sum = 0;
         do{
             Scanner pr = new Scanner(System.in);
-            System.out.print("Introducir precio del producto (valor en número): ");
+            System.out.print("Introducir precio del producto (valor en número): $");
             int producto;
             producto = pr.nextInt();
             sum += producto;
-            System.out.print("*******************\n" + "    " + "Total: " + "   " + sum+"\n********************\n");            
+            System.out.print("********************\n" + "    " + "Total: $" + "  " + sum+"\n********************\n");            
             Scanner ar = new Scanner(System.in);
-            System.out.print("      Introduce R para ingresar otro producto o caulquier otro caracter para detener\n");
+            System.out.print("      Introduce R para ingresar otro producto o cualquier otro caracter para detener\n");
             ra = ar.next();
         }
         while(ra.equals("R"));
